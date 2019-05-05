47b7e1da853123481ac54380ab8608dbec279717
James <jismith1989@googlemail.com>
2019-05-05 19:10:58 +0100

    Create benchmark function

---
+++ validation_test.go
@@ -0,0 +1,25 @@
+package validation
+
+import "testing"
+
+type dummyValidationModel struct {
+	a string   `validation:"required,minlen=4,maxlen=8"`
+	b string   `validation:"required,email,minlen=6,maxlen=5000" name:"Email address"`
+	c []string `validation:"minlen=4,maxlen=8"`
+	d int      `validation:"min=5,max=5000"`
+	e uint     `validation:"min=5,max=5000"`
+	f float64  `validation:"min=0.5,max=5.5"`
+	g float32  `validation:"min=0.5,max=5.5"`
+	h *int     `validation:"required"`
+	i int64    `validation:"min=1,max=9999999"`
+}
+
+func BenchmarkValidation(b *testing.B) {
+	model := dummyValidationModel{}
+
+	for n := 0; n < b.N; n++ {
+		if _, _, err := Validate(model); err != nil {
+			panic(err)
+		}
+	}
+}
